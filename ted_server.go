ffddc5b300e8fe47a35ca5a51b87bbc2f3c46f95
Richard Morrisby <rmorrisby@gmail.com>
2020-12-31 10:01:22 +0000

    Corrections

--- ted_server.go
+++ ted_server.go
@@ -31,8 +31,8 @@ const (
 
 	resultsTable                  = "results"
 	resultsTableColumnDefinitions = "id serial, name varchar(100), testrun varchar(32), category varchar(32), status varchar(32), endtime timestamp with time zone, message varchar(100)"
-	resultsTableCreateSQL         = fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (%s)", resultsTable, resultsTableColumnDefinitions)
-	resultsTableInsertSQL         = fmt.Sprintf("INSERT INTO %s(name, testrun, category, status, endtime, message) VALUES", resultsTable)
+	resultsTableCreateSQL         = "CREATE TABLE IF NOT EXISTS " + resultsTable + " (" + resultsTableColumnDefinitions + ")"
+	resultsTableInsertSQL         = "INSERT INTO " + resultsTable + "(name, testrun, category, status, endtime, message) VALUES"
 )
 
 var isLocal bool // cache the fact that we are running locally (or not)
@@ -98,6 +98,16 @@ func CalcResultCounts(results []structs.Result) {
 	for _, result := range results {
 		IncrementCounts(result)
 	}
+}
+
+var dbConn *sql.DB
+
+func ConnectToDB() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
+	dbConn = db
 }
 
 func IndexPage(w http.ResponseWriter, r *http.Request) {
@@ -235,15 +245,13 @@ func InitResultsCSV() {
 func InitResultsDB() {
 	log.Println("Initialising results DB")
 
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatalf("Error opening database: %q", err)
-	}
+	// dbConn, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	// if err != nil {
+	// 	log.Fatalf("Error opening database: %q", err)
+	// }
 
-	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS results (tick timestamp)"); err != nil {
-		c.String(http.StatusInternalServerError,
-			fmt.Sprintf("Error creating database table: %q", err))
-		return
+	if _, err := dbConn.Exec(resultsTableCreateSQL); err != nil {
+		log.Fatalf("Error creating database table: %q", err)
 	}
 
 	// TODO
@@ -281,7 +289,7 @@ func WriteResultToCSV(result structs.Result) {
 func WriteResultToDB(result structs.Result) {
 	log.Println("Writing result to DB")
 	cmd := resultsTableInsertSQL + fmt.Sprintf("(%s %s %s %s %s %s)", result.Name, result.TestRunIdentifier, result.Category, result.Status, result.Timestamp, result.Message)
-	if _, err := db.Exec(cmd); err != nil {
+	if _, err := dbConn.Exec(cmd); err != nil {
 		log.Fatalf("Error writing result to DB: %q", err)
 	}
 	// TODO
@@ -333,12 +341,14 @@ func ReadResultsCSV() []structs.Result {
 func ReadResultsDB() []structs.Result {
 	log.Println("Reading results from DB")
 
-	rows, err := db.Query(fmt.Sprintf("SELECT * FROM %s", resultsTable))
+	rows, err := dbConn.Query(fmt.Sprintf("SELECT * FROM %s", resultsTable))
 	if err != nil {
-		log.fatalf("Error reading results: %q", err)
+		log.Fatalf("Error reading results: %q", err)
 	}
 
-	log.Printf("Found %d results in DB")
+	cols, _ := rows.Columns()
+	log.Printf("Found %d columns in DB", len(cols))
+	// log.Printf("Found %d results in DB", resultCount)
 
 	// TODO
 	return nil
