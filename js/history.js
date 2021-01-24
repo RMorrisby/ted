c1f8f540cf2593d84f2d481f0d3b11f5f8fa0e8c
Richard Morrisby <rmorrisby@gmail.com>
2021-01-24 18:01:45 +0000

    History page scrolling WIP

--- js/history.js
+++ js/history.js
@@ -5,8 +5,23 @@
 
 // TODO other stuff?
 
-// // On page load, get the existing results
-// // JS requires this function-wrapping
-// window.onload = function () {
-//   getAllResults();
-// };
+function appendKnownIssueButtonsToLastResults() {
+  var table = document.getElementById("history-table-body");
+
+  var last = $(table).find(".child").find("td").last();
+
+  // TODO
+}
+
+function scrollRightMax() {
+  var scrollWidth = $("#history-table-body").scrollWidth;
+  console.log(scrollWidth);
+  $("#history-table-body").scrollLeft(scrollWidth);
+}
+
+// On page load, adorn the table with whstever extra elements we need
+// JS requires this function-wrapping
+window.onload = function () {
+  //   appendKnownIssueButtonsToLastResults();
+  scrollRightMax();
+};
