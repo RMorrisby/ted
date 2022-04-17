3470f128268f8c1888aecc0308ef02b8cba96662
Richard Morrisby <rmorrisby@gmail.com>
2022-04-17 10:41:24 +0100

    Next testrun ID now turns 0.0.9 into 0.0.10

--- pkg/handler/testrun_id_handler.go
+++ pkg/handler/testrun_id_handler.go
@@ -6,6 +6,8 @@ import (
 	"ted/pkg/dataio"
 	"ted/pkg/help"
 
+	"strings"
+
 	"github.com/huandu/xstrings"
 	log "github.com/romana/rlog"
 )
@@ -33,7 +35,32 @@ func TestRunIDNextHandler(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.Method, "GET /testrunid/next called")
 
 		latest := dataio.GetLatestTestRun()
-		next := xstrings.Successor(latest)
+		// next := xstrings.Successor(latest) // this turns 0.0.9 into 0.1.0, but we want 0.0.10
+
+		// Some very manual unpacking & repacking of the version-string
+		// Turn 0.0.9 into 00.00.09, increment, then remove leading zeros
+		var a [3]string
+		for i, x := range strings.Split(latest, ".") {
+			if len(x) == 1 {
+				a[i] = "0" + x
+			} else {
+				a[i] = x
+			}
+		}
+		next := strings.Join(a[:], ".")
+
+		succ := xstrings.Successor(next)
+
+		for i, x := range strings.Split(succ, ".") {
+			if string(x[0]) == "0" {
+				a[i] = string(x[1])
+			} else {
+				a[i] = x
+			}
+
+		}
+		next = strings.Join(a[:], ".") // this is now the next testrun ID
+
 		log.Debugf("Latest test run : %s; next : %s", latest, next)
 		fmt.Fprintf(w, next)
 
